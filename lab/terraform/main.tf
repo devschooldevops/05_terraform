9fc96692511332f15c27e57df970941fd21c39b9
Bogdan Ivan <bogdan.ivan@gmail.com>
2024-09-10 16:48:53 +0300

    Add nginx to the mix

--- lab/terraform/main.tf
+++ lab/terraform/main.tf
@@ -121,6 +121,7 @@ resource "azurerm_linux_virtual_machine" "my_terraform_vm" {
     sku       = "22_04-lts-gen2"
     version   = "latest"
   }
+  custom_data = filebase64("nginx.sh")
 
   computer_name  = var.hostname
   admin_username = var.username
